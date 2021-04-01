5ee6378b791e73d48922d7c930b89cc5a0597295
ArkoC511 <arkoc511@gmail.com>
2021-04-02 01:45:35 +0530

    intial commit

---
+++ plot2.R
@@ -0,0 +1,18 @@
+library("data.table")
+
+workingDirectory <- getwd()
+setwd(workingDirectory)
+
+powerData <- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
+powerData[, globalActivePower := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+powerData[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+powerData <- powerData[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+
+png("plot2.png", width=480, height=480)
+
+## Plot 2
+plot(x = powerData[, dateTime]
+     , y = powerData[, globalActivePower]
+     , type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+dev.off()
